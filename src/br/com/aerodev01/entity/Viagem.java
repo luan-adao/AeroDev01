9557081186434c6a585dd3fcaf85bc1ebbc437ee
luan-adao <luanadao98@gmail.com>
2021-11-03 00:50:15 -0300

    Classe das viagens

---
+++ src/br/com/aerodev01/entity/Viagem.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.aerodev01.entity;
+
+/**
+ *
+ * @author luan
+ */
+public class Viagem {
+    private int id, idAviao;
+    private String origem, destino, data, preco;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the idAviao
+     */
+    public int getIdAviao() {
+        return idAviao;
+    }
+
+    /**
+     * @param idAviao the idAviao to set
+     */
+    public void setIdAviao(int idAviao) {
+        this.idAviao = idAviao;
+    }
+
+    /**
+     * @return the origem
+     */
+    public String getOrigem() {
+        return origem;
+    }
+
+    /**
+     * @param origem the origem to set
+     */
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+
+    /**
+     * @return the destino
+     */
+    public String getDestino() {
+        return destino;
+    }
+
+    /**
+     * @param destino the destino to set
+     */
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    /**
+     * @return the data
+     */
+    public String getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    /**
+     * @return the preco
+     */
+    public String getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+}
