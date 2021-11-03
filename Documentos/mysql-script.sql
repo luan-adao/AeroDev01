78f94f7f39aa96835a819af9925aea6869f5056a
luan-adao <luanadao98@gmail.com>
2021-11-03 00:37:38 -0300

    update

--- Documentos/mysql-script.sql
+++ Documentos/mysql-script.sql
@@ -23,12 +23,12 @@ create table Viagem(
     via_destino varchar(30) not null,
     via_preco decimal(5,2) not null,
     via_data date not null,
-    aviaoID int not null,
-    foreign key (aviaoID) references Aviao (avi_id)
+    via_aviaoID int not null,
+    foreign key (via_aviaoID) references Aviao (avi_id)
 );
 
 insert into Viagem
-(via_origem, via_destino, via_preco, via_data, aviaoID)
+(via_origem, via_destino, via_preco, via_data, via_aviaoID)
 values
 ('SC', 'RS', 100.00, '2021-10-20', 3),
 ('RS', 'PR', 170.00, '2021-10-21', 5),
@@ -40,8 +40,9 @@ values
 ('RS', 'PR', 170.00, '2021-10-27', 3),
 ('RS', 'SC', 100.00, '2021-10-28', 6),
 ('PR', 'SC', 100.00, '2021-10-29', 2),
-('PR', 'RS', 190.00, '2021-10-30', 5);
-
+('PR', 'RS', 190.00, '2021-10-30', 5),
+('PR', 'RS', 190.00, '2021-10-24', 5),
+('PR', 'SC', 100.00, '2021-10-23', 6);
 create table Passageiro(
     pas_cpf varchar(15) primary key,
     pas_nome varchar(50) not null,
@@ -52,32 +53,52 @@ create table Passageiro(
 insert into Passageiro
 (pas_cpf, pas_nome, pas_sexo, pas_cidade)
 values
-('0000000000', 'luan', 'masculino', 'Floripa');
+('0000000000', 'luan', 'masculino', 'Floripa'),
+('123.132.123-11', 'Passageiro01','masculino', 'Floripa' ),
+('123.457.457-32', 'Passageiro02', 'masculino', 'Floripa'),
+('111.222.333-44', 'Passageiro03','masculino', 'Floripa'),
+('936.046.736-04', 'Passageiro04','masculino', 'Floripa'),
+('756.934.999-45', 'Passageiro05','masculino', 'Floripa'),
+('333.222.444-11', 'Passageiro06','masculino', 'Floripa'),
+('987.654.432-10', 'Passageiro07','masculino', 'Floripa'),
+('809.343.212-54', 'Passageiro08','masculino', 'Floripa'),
+('094.543.829-23', 'Passageiro09','masculino', 'Floripa'),
+('923.545.892-45', 'Passageiro10','masculino', 'Floripa');
 
 create Table Funcionario(
 	fun_cpf varchar(15) primary key,
-	fun_nome varchar (50) not null ,
-	fun_senha varchar(50) not null
+	fun_nome varchar (50) NOT NULL ,
+	fun_senha varchar(50) NOT NULL,
+	fun_sexo varchar(15) NOT NULL
 );
 
 insert into Funcionario
-(fun_cpf, fun_nome, fun_senha)
+(fun_cpf, fun_nome, fun_senha, fun_sexo)
 values
-('admin', 'Administrador', '1234');
+('admin', 'Administrador', '1234', 'null');
+
+insert into Funcionario
+(fun_cpf, fun_nome, fun_senha, fun_sexo)
+values
+('999.432.234-00', 'Funcionario01', '123', 'feminino'),
+('043.234.212-45', 'Funcionario02', 'StrongPassword', 'feminino'),
+('123.123.321-76', 'Funcionario03', 'as%dfae*fa@#))asç', 'feminino'),
+('948.444.211-23', 'Funcionario04', 'MeuNome12091989', 'feminino');
+
 
 create table Passagem(
 	psgm_id int primary key auto_increment,
-    psgm_NumeroAssento varchar(4) not null,
-    passageiroID varchar(15),
-    foreign key (passageiroID) references Passageiro(pas_cpf),
-    funcionarioID varchar(15),
-    foreign key (funcionarioID) references Funcionario(fun_cpf),
-    viagemID int,
-    foreign key (viagemID) references Viagem(via_id)
+    psgm_numeroAssento varchar(4) not null,
+    psgm_passageiroID varchar(15),
+    foreign key (psgm_passageiroID) references Passageiro(pas_cpf),
+    psgm_funcionarioID varchar(15),
+    foreign key (psgm_funcionarioID) references Funcionario(fun_cpf),
+    psgm_viagemID int,
+    foreign key (psgm_viagemID) references Viagem(via_id)
 );
-/*
-insert into passagem
-(psgm_numeroassento, passageiroID, funcionarioID, viagemID)
+
+INSERT into Passagem
+(psgm_numeroAssento, psgm_passageiroID, psgm_funcionarioID, psgm_viagemID)
 values
 ('0001', '123.132.123-11', '999.432.234-00', 1),
 ('0023', '123.457.457-32', '123.123.321-76', 12),
@@ -89,11 +110,16 @@ values
 ('0023', '809.343.212-54', '043.234.212-45', 12),
 ('0086', '094.543.829-23', '948.444.211-23', 11),
 ('0008', '923.545.892-45', '043.234.212-45', 4);
-*/
+
 create table Cancelamento(
     id int primary key auto_increment,
     PassagemID int not null unique,
     foreign key (PassagemID) references Passagem(psgm_id)
+);
+
+CREATE TABLE AssentoOcupado(
+	aso_id int PRIMARY KEY auto_increment,
+	aso_passagemAssento int not null
 );
 
 /*
