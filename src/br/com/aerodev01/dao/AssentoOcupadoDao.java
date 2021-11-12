5fd4be23f92d28b282b9006a6da6bff798272c6f
luan-adao <luanadao98@gmail.com>
2021-11-12 18:12:47 -0300

    DAO

--- src/br/com/aerodev01/dao/AssentoOcupadoDao.java
+++ src/br/com/aerodev01/dao/AssentoOcupadoDao.java
@@ -11,6 +11,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -25,16 +28,37 @@ public class AssentoOcupadoDao implements Serializable{
         try {
             con = ConnectionFactory.getConnection();
             String sql = "INSERT INTO AssentoOcupado(aso_viagemID, aso_assentoNumero) VALUES (?, ?)";
-            stmt = con.prepareStatement(sql);
+            stmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             stmt.setInt(1, aso.getIdViagem());
             stmt.setInt(2, aso.getNumeroAssento());
             stmt.executeUpdate();
+            rs = stmt.getGeneratedKeys();
             rs.next();
+            aso.setId(rs.getInt(1));
         } catch (SQLException e) {
             System.err.println("Ocorreu um erro ao salvar " + e.getMessage());
         } finally {
             ConnectionFactory.closeConnection(con, stmt, rs);
         }
+    }
+    
+    public List retornaAssentos(int idViagem) {
+        List lista = new ArrayList();
+        try {
+            con = ConnectionFactory.getConnection();
+            String sql = "SELECT aso_assentoNumero FROM AssentoOcupado where aso_viagemID=?";
+            stmt = con.prepareStatement(sql);
+            stmt.setInt(1, idViagem);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                lista.add(rs.getInt("aso_assentoNumero"));
+            }
+        } catch (Exception e) {
+            System.err.println("Erro ao listar assentos " + e.getMessage());
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt, rs);
+        }
+        return lista;
     }
     
     
