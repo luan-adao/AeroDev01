dfb216bbef7ebc2ca92750ed5cba103ac9183fda
luan-adao <luanadao98@gmail.com>
2021-11-14 21:16:51 -0300

    Update

--- src/br/com/aerodev01/entity/Cancelar.java
+++ src/br/com/aerodev01/entity/Cancelar.java
@@ -10,5 +10,33 @@ package br.com.aerodev01.entity;
  * @author luan
  */
 public class Cancelar {
-    
+    private int id, passagemID;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the passagemID
+     */
+    public int getPassagemID() {
+        return passagemID;
+    }
+
+    /**
+     * @param passagemID the passagemID to set
+     */
+    public void setPassagemID(int passagemID) {
+        this.passagemID = passagemID;
+    }
 }
--- src/br/com/aerodev01/view/Cancelar.java
+++ src/br/com/aerodev01/view/Cancelar.java
@@ -5,7 +5,10 @@
  */
 package br.com.aerodev01.view;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  *
@@ -15,6 +18,20 @@ public class Cancelar {
     public Cancelar(JFrame frame) {
         Window janela = new Window(true);
         JPanel painel = new JPanel();
+        painel.setLayout(null);
+        
+        janela.addLabelTitulo(painel, "Cancelar", 210, 250, 20);
+        janela.addLabel(painel, "Código:", 100, 150, 100, 20);
+        JTextField tfieldCodigo = janela.addTextField(painel, false, 100, 180, 50, 20);
+        JButton btnChecar = janela.addButton(painel, "Checar", 155, 180, 90, 20);
+        JButton btnCancelar = janela.addButton(painel, "Cancelar", 0, 0, 0, 0);
+        
+        janela.getContentPane().add(painel);
+        janela.setVisible(true);
     }
     
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        new Cancelar(f);
+    }
 }
